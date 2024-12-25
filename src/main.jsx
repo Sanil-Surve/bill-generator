3b66d4eddf72570a0c48dbcd230718ea2077d73a
Sanil-Surve <sanilsurve42@gmail.com>
2024-12-25 10:02:02 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+// createRoot(document.getElementById("root")).render(<App />);
+
+
+import { createRoot }from 'react-dom/client';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
